910c35bc9317dbfa36d3c1d9161e769ac471186b
SLY <IT_HEAD@ITs-MacBook-Pro.local>
2024-11-27 11:27:36 +0000

    HrmanagerSection  and Employee  Personal details added

--- scripts/env.js
+++ scripts/env.js
@@ -1,6 +1,5 @@
 // env.js
 const ENV = {
     API_URL: 'http://localhost:5000/api',
-    API_DEPLOY_URL: 'https://employeerecordsystembackend.onrender.com',
+    API_DEPLOY_URL: 'https://employeerecordsystembackend.onrender.com/api',
   };
-  
